42986822e2bedb8b089c01ed2744760f831902c9
sushil016 <sahanisushil325@gmail.com>
2024-10-12 19:39:43 +0530

    build faliure fixed

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Sun, Moon } from 'lucide-react';
+// import React from 'react';
+// import { Sun, Moon } from 'lucide-react';
 
-// interface ThemeSwitchProps {
-//   isDarkTheme: boolean;
-//   toggleTheme: () => void;
-// }
+// // interface ThemeSwitchProps {
+// //   isDarkTheme: boolean;
+// //   toggleTheme: () => void;
+// // }
 
-const ThemeSwitch: React.FC = () => {
-  return (
-    <button
-      onClick={toggleTheme}
-      className="fixed top-20 right-4 z-50 p-2 rounded-full bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200"
-    >
-      {isDarkTheme ? <Sun className="w-6 h-6" /> : <Moon className="w-6 h-6" />}
-    </button>
-  );
-};
+// const ThemeSwitch: React.FC = ({ isDarkTheme, toggleTheme }) => {
+//   return (
+//     <button
+//       onClick={toggleTheme}
+//       className="fixed top-20 right-4 z-50 p-2 rounded-full bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200"
+//     >
+//       {isDarkTheme ? <Sun className="w-6 h-6" /> : <Moon className="w-6 h-6" />}
+//     </button>
+//   );
+// };
 
-export default ThemeSwitch;
+// export default ThemeSwitch;
