1f09b48f4f4cc67220ee0ba9a4d58b338467a6b4
sushil016 <sahanisushil325@gmail.com>
2024-10-28 17:49:12 +0530

    added custom cursor

--- src/App.tsx
+++ src/App.tsx
@@ -15,13 +15,57 @@ import ApplicationForm from './components/community/ApplicationForm';
 import AdminDashboard from './components/community/AdminDashboard';
 import ClubMembershipForm from './components/community/MemberDetails';
 import AdminDashboardMember from './components/community/MemberInfoPage';
-
+import useMousePosition from './components/hooks/mousePosition';
+import { motion } from "framer-motion";
 
 
 
 function App() {
   const [scrollPosition, setScrollPosition] = useState(0);
-  
+  const { x, y } = useMousePosition();
+  const [bgVariant, setBgVariant] = useState("default");
+
+  const [cursorText, setCursorText] = useState("");
+
+  const variants = {
+    default: {
+      x: x - 20,
+      y: y - 20,
+      transition: {
+        type: "spring",
+        mass: 0.6,
+      },
+    },
+
+    navbarCursor: {
+      opacity: 1,
+      width: 60,
+      height: 60,
+      border: "none",
+      backgroundColor: "#FFA500",
+      mixBlendMode: "difference" as const,
+      x: x - 30,
+      y: y - 30,
+    },
+    nameCursor: {
+      opacity: 1,
+      width: 100,
+      height: 100,
+      // border:"none",
+      backgroundColor: "#1E1E2E",
+      mixBlendMode: "difference" as const,
+      x: x - 50,
+      y: y - 50,
+      cursor: "none",
+    },
+  };
+
+  const spring = {
+    type: "spring",
+    stiffness: 500,
+    damping: 28,
+  };
+
 
   useEffect(() => {
     const handleScroll = () => {
@@ -42,6 +86,18 @@ function App() {
   return (
     <Router>
       <div className={`min-h-screen  text-white`}>
+      <div>
+        <motion.div
+          variants={variants}
+          transition={spring}
+          animate={bgVariant}
+          className="cursor-none z-50 h-[40px] w-[40px] rounded-full border-2 border-orange-600 fixed top-0 left-0 pointer-events-none justify-center items-center shadow-lg shadow-violet-500"
+        > 
+          <span className="text-white m-auto font-size-inherit flex justify-center items-center mt-9">
+            {cursorText}
+          </span>
+        </motion.div>
+      </div>
         <div className="lightning-robot-bg"></div>
         <Header  />
         {/* <ThemeSwitch /> */}
