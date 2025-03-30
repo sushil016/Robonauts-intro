b100608f06aaa9a54ff5bd97c6aaf7f365f30962
sushil016 <sahanisushil325@gmail.com>
2025-03-30 16:27:52 +0530

    updated home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
-import { Cpu } from 'lucide-react';
+
 import { motion } from 'framer-motion';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
